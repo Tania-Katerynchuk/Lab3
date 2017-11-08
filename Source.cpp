a604cea5ba6937ee8879a250eeb6c9863e017e5c
Tania-Katerynchuk <32957312+Tania-Katerynchuk@users.noreply.github.com>
2017-11-08 21:41:36 +0200

    1

--- Source.cpp
+++ Source.cpp
@@ -7,7 +7,7 @@
 #include <vector>
 
 using namespace std;
-
+void DeletePerson(vector<vector <char>>& line, int& n);
 bool OpenFile(vector<vector<char>>& line,int& n);
 
 //відкриття файла
@@ -53,20 +53,94 @@ void WriteLeftLine(vector<char> line, char fileName[61]) {
 
 void OutputLine(vector<vector <char>> line,int n) {
 	cout << "Файл вiдкрився для зчитування iнформацiї.\nВміст файла:" << endl;
+	cout << "________________________________________________________________________________" << endl;
+	cout << "Прiзвище |Iм'я |По батьковi |Факультет |Спецiальність батька |матерi |Кiлькiсть братiв i сестер|" << endl;
+	cout << "________________________________________________________________________________" << endl;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < line[i].size(); j++) {
 			if (line[i][j] == ';') {
-				cout << "|";
+				cout << " |";
 
 			}
 			else
 				cout << line[i][j];
 		}
-		cout <<"|"<< endl;
+		cout <<" |"<< endl;
+		cout << "________________________________________________________________________________" << endl;
 		
 	}
-
 }
+
+void DeletePerson(vector<vector <char>>& line, int& n) {
+	char namePerson[20];
+	cin>>namePerson;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < line[i].size(); j++) {
+			if (line[i][j] == ';') {
+				int f=0;
+				for (int k = 0; k < 5; k++) {
+					
+					if (namePerson[k] == line[i][k]) {
+						f++;
+					}
+				}
+				if (f == j - 1) {
+					line[i].clear();
+					for (int k = i; k < n; k++) {
+						line[i] = line[i + 1];
+					}
+					line[n - 1].clear();
+					n--;
+				}
+				
+				break;
+			
+			}
+		}
+	}
+}
+
+void ThrowData(vector<vector <char>>& line, int& n, string data) {
+	for (int i = 0; i < data.size(); i++) {
+		line[n].push_back(data[i]);
+	}
+}
+
+
+void AddPerson(vector<vector <char>>& line, int& n) {
+	line.push_back(vector<char>());
+	string data;
+	cout << "Введiть прiзвищe: ";
+	cin >> data;
+	ThrowData(line, n,data);
+	line[n].push_back(';');
+	cout << "Введiть iм'я: ";
+	cin >> data;
+	ThrowData(line, n, data);
+	line[n].push_back(';');
+	cout << "Введiть по батьковi: ";
+	cin >> data;
+	ThrowData(line, n, data);
+	line[n].push_back(';');
+	cout << "Факультет: ";
+	cin >> data;
+	ThrowData(line, n, data);
+	line[n].push_back(';');
+	cout << "Введiть спецiальнiсть батька: ";
+	cin >> data;
+	ThrowData(line, n, data);
+	line[n].push_back(';');
+	cout << "Введiть спецiальнiсть матерi: ";
+	cin >> data;
+	ThrowData(line, n, data);
+	line[n].push_back(';');
+	cout << "Введiть кiлькiсть сестер i братiв: ";
+	cin >> data;
+	ThrowData(line, n, data); 
+	n++;
+}
+
+
 
 
 
@@ -74,14 +148,29 @@ int main() {
 	setlocale(LC_CTYPE, "ukr");
 	vector<vector <char>> line;
 	line.push_back(vector<char>());
-	//vector < vector <double> > tmp1(sizeMatrix, vector<double>(sizeMatrix);
 	//Відкриваємо файл
 	int n = 0;
-	OpenFile(line,n);
-	//if (OpenFile(line,n)) {
+	int menu=0;
+	if (OpenFile(line,n)) {
 		OutputLine(line,n);//вивід даних з файла
+		cout << "1-Видалення даних про задану персону\n2-Додання даних про нову персону\n";
+		AddPerson(line, n);
+		OutputLine(line, n);
+		/*switch menu {
+		case 1:
+			cout<<"Введіть прiзвище персони данi якої ви хочете видалити: "
+			DeletePerson(line, n);
+			OutputLine(line, n);
+			break;
+		case 2:
+			(line, n);
+			OutputLine(line, n);
+			break;
+		}*/
 		
-	//}
+		
+	}
+
 	system("pause");
 	return 0;
 }
